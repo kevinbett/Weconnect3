e7e1d9f9d5266a7fceb3aea7aedc0890ba02ae01
kevinbett <bettkevin757@gmail.com>
2018-03-07 02:22:04 +0300

    Ammends auth.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from instance.config import app_config
-from api.v1.auth import auth as users_blueprint
+from api.v1.auth import auth as auth_blueprint
 from api.v1.business import business_blueprint
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.register_blueprint(users_blueprint, url_prefix="/auth")
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
     app.register_blueprint(business_blueprint, url_prefix="/business")
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
