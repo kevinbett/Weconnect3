abbfcc80a8b3b2bd788e49177f62314c78349feb
kevinbett <bettkevin757@gmail.com>
2018-03-18 17:30:00 +0300

    feature(Models): Adds SQLALchemy models
    This commit add User, Business and Reviews SQLALchemy Models in
    models.py
    
    [Delivers #156041163]

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from instance.config import app_config
 from api.v1.auth import auth as auth_blueprint
 from api.v1.business import business_blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -9,7 +14,5 @@ def create_app(config_name):
     app.register_blueprint(business_blueprint)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-
+    db.init_app(app)
     return app
-
-
