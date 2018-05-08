78faa212ea50862ddb3e44b43444e6062f2b7a9a
kevinbett <bettkevin757@gmail.com>
2018-05-09 01:08:55 +0300

    [Feature #155832410] Adds authentication tests

---
+++ tests/test_auth.py
@@ -0,0 +1,34 @@
+from tests.test import BaseTestCase
+from flask import json
+
+class AuthTestCase(BaseTestCase):
+
+    def test_register(self):
+        self.client().post('/register', data=json.dumps(self.user), content_type="application/json")
+        user = self.user
+        user["email"] = "user@example.com"
+        response = self.client().post('/register', data=json.dumps(self.user), content_type="application/json")
+        self.assertEquals("email is already in use", json.loads(response.data)["message"])
+
+    def test_login(self):
+        self.client().post('/register', data=json.dumps(self.user), content_type="application/json")
+        response = self.client().post('/login', data=json.dumps(self.user), content_type="application/json")
+        self.assertEquals("You are now logged in as test_user", json.loads(response.data)["message"])
+
+
+    def test_empty_login_details(self):
+        self.client().post('/register', data=json.dumps(self.user), content_type="application/json")
+        user = self.user
+        user["password"] = ""
+        response = self.client().post('login', data=json.dumps(user), content_type="application/json")
+        self.assertEquals(["Your password should have atleast 6 characters"], json.loads(response.data)["message"])
+
+
+    def test_invalid_login_details(self):
+        self.client().post('/register', data=json.dumps(self.user), content_type="application/json")
+        user = {
+            "email": "user@example.com",
+            "password": "wrongpassword"
+        }
+        response = self.client().post('login', data=json.dumps(user), content_type="application/json")
+        self.assertEquals("The user name or password provided is wrong", json.loads(response.data)["message"])
