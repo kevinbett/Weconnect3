3633ffe75d7a00f99e13042a36ccf7bcdf9a9e27
kevinbett <bettkevin757@gmail.com>
2018-05-09 00:47:17 +0300

    [feature #155584538] Adds 400, 404 and 405 error handlign

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,8 @@ from instance.config import app_config
 from api.v1.auth import auth as auth_blueprint
 from api.v1.business import business_blueprint
 from flask_sqlalchemy import SQLAlchemy
+from api.global_functions import response_message
+
 
 db = SQLAlchemy()
 
@@ -14,5 +16,20 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return response_message("Page not found", 404)
+
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        return response_message("Method not allowed", 405)
+
+    @app.errorhandler(400)
+    def bad_request(e):
+        return response_message("Bad request", 400)
+
     return app
+
+
 
