9c8f7d822c61a1be153385baa6869551618ef8ec
kevinbett <bettkevin757@gmail.com>
2018-05-10 12:16:00 +0300

    [Feature #155832275] Adds get_user authentication token

--- api/global_functions.py
+++ api/global_functions.py
@@ -7,3 +7,20 @@ def response_message(message, status_code=200):
     }
     return jsonify(response), status_code
 
+def get_user(auth_token):
+    from api.models import User
+
+    if auth_token:
+        token = auth_token.split(" ")[1]
+        user_id = User.decode_auth_token(token)
+
+        if not isinstance(user_id, str):
+            user = User.query.filter_by(id=user_id).first()
+            return user
+        return {
+            'message': user_id
+        }
+
+    return {
+        'message':'provide a valid token'
+    }
