7e7acfd560b06ca5ea6b8243a5108e5db7d4b240
kevinbett <bettkevin757@gmail.com>
2018-05-10 12:55:37 +0300

    [Feature #156041163] Adds Secret Key

--- instance/config.py
+++ instance/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
+    SECRET = os.getenv('SECRET') or 'this_is%very&secret*dont_tell$anyone'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:andela@localhost/weconnect'
 
 class DevelopmentConfig(Config):
