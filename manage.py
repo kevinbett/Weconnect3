139d0f6e108742a9019e6b77e10d0ae5f9b92674
kevinbett <bettkevin757@gmail.com>
2018-05-10 12:57:59 +0300

    [Bug] Removed api import models bug

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from api import db, create_app
-from api import  models
 
 config_name = os.getenv('APP_ENV') # config_name = "development"
 app = create_app("development")
