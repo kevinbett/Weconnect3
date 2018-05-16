28ef6b84fef9a61fb4c6efede012cb42584c1d0f
kevinbett <bettkevin757@gmail.com>
2018-05-16 10:45:54 +0300

    [Feature #155559262] Adds migrate

--- manage.py
+++ manage.py
@@ -4,11 +4,12 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from api import db, create_app
 
+
 config_name = os.getenv('APP_ENV') # config_name = "development"
 app = create_app("development")
 
-migrate = Migrate(app,db)
 manager = Manager(app)
+migrate = Migrate(app,db)
 
 manager.add_command("db",MigrateCommand)
 
