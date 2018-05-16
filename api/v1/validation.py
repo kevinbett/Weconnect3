fa238ae3f27c93e9a7575089e3cc0cbbf8d17ea7
kevinbett <bettkevin757@gmail.com>
2018-05-16 10:43:59 +0300

    [Feature #155559262] Appends lower cases for user inputs

--- api/v1/validation.py
+++ api/v1/validation.py
@@ -5,7 +5,7 @@ def check_name(name):
     if re.match("^$", name.strip()):
         raise Exception("You have entered an invalid name")
     else:
-        return name
+        return name.lower()
 
 
 def check_password(password):
@@ -17,27 +17,27 @@ def check_password(password):
 def check_email(email):
     if not re.match(r"(^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-z]+$)", email):
         raise Exception("You have not entered a valid email")
-    return email
+    return email.lower()
 
 
 def check_review(review):
     if re.match("^$", review.strip()):
         raise Exception("You have entered an invalid review")
-    return review
+    return review.lower()
 
 
 def check_business(name):
     if re.match("^$", name.strip()):
         raise Exception("Business name or type cannot be blank")
     else:
-        return name
+        return name.lower()
 
 def check_update(name):
     try:
         if re.match("^$", name.strip()):
             raise Exception("Field cannot be empty")
         else:
-            return name
+            return name.lower()
 
     except AttributeError:
         return ""
