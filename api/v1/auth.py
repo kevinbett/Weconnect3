d7ebdb2acd8d8ad6c8d5ad83131358422c4c1f78
kevinbett <bettkevin757@gmail.com>
2018-05-17 11:49:33 +0300

    [Feature #157422290] Adds reset password feature

--- api/v1/auth.py
+++ api/v1/auth.py
@@ -75,5 +75,67 @@ def change_password():
 
     return response_message("Password has been succesfully changed", status_code=200)
 
-# @auth.route('/reset-password', methods=['POST'])
-# def reset_password():
+
+@auth.route('/reset-password', methods=['POST'])
+def reset_password():
+    requestData = request.get_json()
+    try:
+        email = check_email(requestData.get('email'))
+    except Exception:
+        return response_message("Enter a valid email", status_code=400)
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        auth_token = user.encode_auth_token(user.id)
+
+        if auth_token:
+            link = "http://127.0.0.1:5000/reset-password/{}".format(auth_token.decode())
+            res = {
+                "message": "Reset your password from the provided token",
+                "link": link
+            }
+            return jsonify(res), 200
+
+
+@auth.route('/reset-password/<token>', methods=['POST'])
+def new_password(token):
+    requestData = request.get_json()
+    try:
+        new_password = check_password(requestData.get('new_password'))
+    except Exception:
+        return response_message("Enter a valid password", status_code=400)
+    user = get_user(token, split_token=False)
+    if not isinstance(user, User):
+        return response_message(user, 401)
+
+    user.set_password(new_password)
+    user.save()
+
+    return response_message("Password has been successfully changed", status_code=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@auth.route('/logout', methods=['POST'])
+def log_out():
+    auth_token = request.headers.get("Authorization")
+    user = get_user(auth_token)
+    if not isinstance(user, User):
+        return response_message(user, 401)
+
+    user = User
