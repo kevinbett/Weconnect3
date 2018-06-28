4ecf8c59a646f6f6f4313d4767ff879b506e73d6
kevinbett <bettkevin757@gmail.com>
2018-06-28 11:49:21 +0300

    Adds Cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from instance.config import app_config
 from api.v1.auth import auth as auth_blueprint
 from api.v1.business import business_blueprint
@@ -7,6 +8,7 @@ from api.models import db
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(business_blueprint)
     app.config.from_object(app_config[config_name])
